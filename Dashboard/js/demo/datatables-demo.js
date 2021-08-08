1bf1207fb8f3edba82b617448dfe00e5984b4ba7
vipulkumarpandey111 <vipulkumarpandey111@gmail.com>
2021-08-08 19:45:04 +0530

    first commit

---
+++ Dashboard/js/demo/datatables-demo.js
@@ -0,0 +1,14 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable();
+});
+$(document).ready(function() {
+  $('#myPlan').DataTable();
+});
+
+$(document).ready(function() {
+  $('#payment').DataTable();
+});
+$(document).ready(function() {
+  $('#myVideo').DataTable();
+});
